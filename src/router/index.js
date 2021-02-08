aa91320aff18aa25ad96862a42cf308bc1833e72
EC2 Default User <ec2-user@ip-172-31-39-177.ap-northeast-1.compute.internal>
2021-02-08 05:36:10 +0000

    built application

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,16 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+  const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: () => import('../views/Contact.vue')
   },
   {
     path: '/about',
@@ -16,7 +21,12 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import('../views/About.vue')
+  },
+  {
+    path:'/test',
+    name:'Test',
+    component:() => import('../views/test.vue')
   }
 ]
 
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="about">
+  <div class="test">
     <h1>This is an about page</h1>
     <h1>何かをいれるかな？</h1>
-    <h1>test</h1>
   </div>
 </template>
